1fc7389e1a7a7d94f8863dd7925ec8906cc65d04
dianisia <dianisia.m@yandex.ru>
2021-03-21 19:55:47 +0300

    Init project

---
+++ SpotifyClone/SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Диана Мансурова on 21.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
