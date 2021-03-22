36ea399da6edeb4148e4e9acceb257843b4295a9
dianisia <dianisia.m@yandex.ru>
2021-03-22 22:21:03 +0300

    Start auth and fetching tokens

--- SpotifyClone/SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/SpotifyClone/Managers/AuthManager.swift
@@ -9,8 +9,26 @@ import Foundation
 
 final class AuthManager {
     static let shared = AuthManager()
+    
+    struct Constants {
+        static let clientId = "a0e72a1c68054f889648a2e911192a6d"
+        static let clientSecret = "afb62ddc632c4653bbd0bb6d137cfb2b"
+    }
 
     private init() {}
+
+    public var signInURL: URL? {
+        let baseURL = "https://accounts.spotify.com/authorize"
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let string =
+                """
+                \(baseURL)?response_type=code&client_id=\(Constants
+                        .clientId)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE
+                """
+        return URL(string: string)
+
+    }
 
     var isSignedIn: Bool {
         return false
@@ -30,5 +48,20 @@ final class AuthManager {
 
     private var shouldRefreshToken: Bool {
         return false
+    }
+
+    public func exchangeCodeForToken(
+            code: String,
+            completion: @escaping ((Bool) -> Void)
+    ) {
+
+    }
+
+    private func cacheToken() {
+
+    }
+
+    private func refreshAccessToken() {
+
     }
 }
