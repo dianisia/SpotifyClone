6e19dbacce02e53d59104c36c93bd81cb79bb905
dianisia <dianisia.m@yandex.ru>
2021-03-23 23:09:46 +0300

    Add settings screen

---
+++ SpotifyClone/SpotifyClone/Models/SettingsModels.swift
@@ -0,0 +1,15 @@
+//
+// Created by Диана Мансурова on 23.03.2021.
+//
+
+import Foundation
+
+struct Section {
+    let title: String
+    let options: [Option]
+}
+
+struct Option {
+    let title: String
+    let handler: () -> Void
+}
