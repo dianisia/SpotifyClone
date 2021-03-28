d10989980f2c13f3674151cb3f57755b1dbc89dd
dianisia <dianisia.m@yandex.ru>
2021-03-28 23:17:34 +0300

    Start dealing with UICollectionView

---
+++ SpotifyClone/SpotifyClone/Views/Browse/RecommendedTrackCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+// Created by Диана Мансурова on 28.03.2021.
+//
+import UIKit
+import Foundation
+
+class RecommendedTrackCollectionViewCell: UICollectionViewCell {
+    static let identifier = "RecommendedTrackCollectionViewCell"
+}
